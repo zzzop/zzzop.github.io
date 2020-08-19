d1c7888ec9a4686343df60641659a3d3564c204d
zzzop <sczan110@gmail.com>
2020-08-19 19:49:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
+
+let countDown = new Date("Dec 20, 2020 00:00:00").getTime(),
+  x = setInterval(function () {
+    let now = new Date().getTime(),
+      distance = countDown - now;
+
+    (document.getElementById("days").innerText = Math.floor(distance / day)),
+      (document.getElementById("hours").innerText = Math.floor(
+        (distance % day) / hour
+      )),
+      (document.getElementById("minutes").innerText = Math.floor(
+        (distance % hour) / minute
+      )),
+      (document.getElementById("seconds").innerText = Math.floor(
+        (distance % minute) / second
+      ));
+
+    //let distance = now;
+
+    (document.getElementById("hours1").innerText = Math.floor(
+      (now % day) / hour + 8
+    )),
+      (document.getElementById("minutes1").innerText = Math.floor(
+        (now % hour) / minute
+      )),
+      (document.getElementById("seconds1").innerText = Math.floor(
+        (now % minute) / second
+      ));
+
+    //do something later when date is reached
+    //if (distance < 0) {
+    //  clearInterval(x);
+    //  'IT'S MY BIRTHDAY!;
+    //}
+  }, second);
