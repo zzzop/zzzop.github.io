c6cbb58038fe666221784c6f6de6a7b9fb9c0b22
zzzop <sczan110@gmail.com>
2020-09-04 16:32:23 +0800

    Update script.js
    hhhh

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date("Dec 20, 2020 00:00:00").getTime(),
+let countDown = new Date("Dec 25, 2020 21:00:00").getTime(),
   x = setInterval(function () {
     let now = new Date().getTime(),
       distance = countDown - now;
